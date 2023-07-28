0952bf46a08d8d8f55cad0ec53170454040dc82b
venkatashok01 <venkatashok2001@gmail.com>
2023-07-28 19:32:27 +0530

    first commit

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city){
-    const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-
+    const api_key = "bd4ca218c56aa15f59b3aed5365a8752";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
+    console.log(url);
     const weather_data = await fetch(`${url}`).then(response => response.json());
 
 
